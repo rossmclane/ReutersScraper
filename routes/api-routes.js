981d74e72b5bd2e85a2c9555bfeb68f2d96bbe60
Ross McLane <ross.mclane@gmail.com>
2019-12-17 21:58:48 -0500

    Check scraping functionality

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -27,7 +27,9 @@ module.exports = function(app) {
         };
 
         db.Article.create(result)
-          .then(function(dbArticle) {})
+          .then(function(dbArticle) {
+            console.log(dbArticle);
+          })
           .catch(function(err) {
             console.log(err);
           });
